df60cda2d7605228508f6bd233a285491d8cdc39
Will G Piro <willgpiro@gmail.com>
2020-02-04 10:57:35 -0800

    Updated pseudo code

--- common/functions.js
+++ common/functions.js
@@ -1,16 +1,46 @@
 //Define user information. Create function that pulls information from the USER form. EventListeners will need to be attached to each of the radio buttons, one for race and gender identity. 
 
+
+//USER CREATION PAGE
 //Original state (i.e. health stats, wealth stats, and umobility stats) will need to be represented on screen already. 
 
-//Once user selects race and gender identity state will be manipulated on screen. 
+
+//FUNCTION BEGINS
+
+//User will enter a name. Need input form. 
+
+//Once user selects race and gender identity and regional location, then state will be manipulated on screen. These selections will happen via radio button.
+
+//Information will be put into local storage (stringify JSON)
+
+//The state for the first Question will be pulled after user updates new state. 
 
 //grab user-information from local storage. 
 
 //The user-information must be parsed from JSON. 
 
-//The state for the first Question will be pulled froNm original state. 
-  //
+//======================================================================
 
-// Questions are assigned an id that correlates with the id in our life event objects.
+//GAME PAGE
 
-//Once the 
+//Display prompt
+
+//Dispaly prompt image
+
+//Dispaly user information (state)
+
+//Show options (via radio buttons)
+
+//Create submit button
+
+//Create select option function for user choice.
+
+//On submit the results from the users selection will be shown. Information will be pulled from our data object. Questions are assigned an id that correlates with the id in our life event objects.
+
+//When user hit submit button update local storage. 
+
+//Create user button that will generate next question. 
+
+//Display new prompt, new image, new responses. 
+//===============================================================================
+
