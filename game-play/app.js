c133a744c0669dd70fb851aff7c5c9cea6de7c95
Will G Piro <willgpiro@gmail.com>
2020-02-05 10:49:12 -0800

    added question objects 4 and 5

--- game-play/app.js
+++ game-play/app.js
@@ -1,11 +1,15 @@
 // import all question objects
 
-
+  //Need specific objects (i.e. import 'race' object) Look at Colls file structure. 
 
 
 // get user from local storage
+    //Look at JSON function.
 
 // get query parameter from URL (this assumes that initial query parameter has been set to race)
+    //We would need to pull a specific ID from the splash page. 
+
+
 
 // get question id from URL and store as variable
 
@@ -14,25 +18,28 @@
 // get form from DOM
 // get results elements from DOM
 
-
-
 // displayGame()
 
     // ensure that first three questions are 'user profile' questions; then ask remaining questions at random
 
-        // create array of asked questions
+    //We will iterate through an array. 
 
+        // create array of asked questions
+            //Coll has outlined this type of array in lifeEvents.js and medicalIssueOutcomes.js
+
+            ==================================================================================== TABLEED
         // set question array in local storage
+            //We have function to acomplish this. 
 
         // if race is not in the array, then pass in raceObject; otherwise, pass in genderObject
+            //
 
         // if gender is not in the array, then pass in genderObject; otherwise, pass in regionObject
 
         // if region is not in the array, then pass in regionObject; otherwise, use getRandomQuestion function (passing in lifeEvents array) to randomly assign a question
-
+==========================================================================================================================================
     
-
-// event listener on form submission
+// event listener on form submission -- Will look at the Event Listener - Needs to update state -
 
     // prevent default behavior
 
@@ -41,8 +48,9 @@
     // use formData object to get data for generated prompt
 
     // updateUserObject() function:
-        // update user object based on choice
+        // update user object based on choice -- 
 
+    
     // renderChart() function:
         // Will update chart based on choice (note: we will need two arrays: one for labels, the other with the corresponding scores)
 
@@ -57,3 +65,5 @@
     // push mutated user object back into local storage
 
     // if there are no remaining questions, go to results page
+
+//The continue button will generate the next question.
