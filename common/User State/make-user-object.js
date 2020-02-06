03808f1ac2f1851326746ebe0e66a10b1533784e
dorjepadma <dorjepadma@gmail.com>
2020-02-05 17:00:00 -0800

    function user-update

--- common/User State/make-user-object.js
+++ common/User State/make-user-object.js
@@ -1,26 +1,45 @@
-export default function makeUser(formData) {
+function makeUser(formData) {
     // make user object
-    const user = {
-        name: formData.get('name'),
-        race: formData.get('race'),
-        gender: formData.get('gender'),
-        region: formData.get('region'),
-        wealth: '',
-        health: '',
-        uMobility: '',
+    name: formData.get('name'),
+    if (formData === 'white') {
+        const user = {
+        health: 91,
+        wealth: 100,
+        uMobility: 100,
+    }} else if (formData === 'black') {
+        const user = {
+        health:87,
+        money:80,
+        mobility:60, 
+    }} else if (formData === 'asian') {
+        const user ={
+        health:100,
+        money:100,
+        mobility:100,
+    }} else if (formData === 'native american') {
+        const user ={
+        health:76,
+        money:70,
+        mobility:50,
+    }} else if (formData === 'Hispanic or Latino') {
+        const user ={
+        health:96,
+        money:80,
+        mobility:75,
+    }} else if (formData === 'Two or More Races') {
+        const user ={
+        health:92,
+        money:80,
+        mobility:80,
+    }} else if (formData === 'Native Hawaiian or Other Pacific Islander') {
+        const user ={
+        health:80,
+        money:75,
+        mobility:75,
+        }
     };
     return user;
 }
 
-    // make user object
-export const user = {
-    name: 'john',
-    race: 'white',
-    gender: 'male',
-    region: 'south',
-    wealth: 40,
-    health: 10,
-    uMobility: 30,
-};
-   
+
 
