867524cf26925f1cece18a0ad7ab040cb43e8798
Dannie Schumaker <draeschumaker@gmail.com>
2020-02-07 10:27:38 -0800

    changimage in results box

--- game-play/display-results.js
+++ game-play/display-results.js
@@ -1,9 +1,11 @@
 export default function displayResults(choice, lifeEvent) {
     // connect textContent of heading and paragraph to corresponding result
     const resultSection = document.getElementById('result-section');
+    const lifeEventImage = document.getElementById('life-event-image');
     resultSection.style.display = 'flex';
     const resultsHeader = document.getElementById('results-header');
     const resultsDescription = document.getElementById('results-description');
+    lifeEventImage.src = `../assets/diversity-images/${lifeEvent.image}`;
     resultsHeader.textContent = `You selected: ${choice}`;
     resultsDescription.textContent = lifeEvent.result;
 }
--- game-play/life-events.js
+++ game-play/life-events.js
@@ -3,7 +3,7 @@ const race = {
 
     id: 'race',
     title: 'The Human Race',
-    image:'',
+    image:'race.jpeg',
     description:`
     Race and ethnicity are socially constructed categories that have tangible effects on the lives of individuals who are defined by how one perceives one's self and how one is perceived by others. It is important to acknowledge the social construction (i.e., created from prevailing social perceptions, historical policies, and practices) of the concepts of race and ethnicity because it has implications for how measures of race have been used and changed over time. Furthermore, the concept of race is complex, with a rich history of scientific and philosophical debate as to the nature of race (James, 2016). Racial and ethnic disparities are arguably the most obstinate inequities in health over time, despite the many strides that have been made to improve health in the United States. Moreover, race and ethnicity are extremely salient factors when examining health inequity (Bell and Lee, 2011; Smedley et al., 2008; Williams et al., 2010). Therefore, solutions for health equity need to take into account the social, political, and historical context of race and ethnicity in this country.
 
@@ -27,7 +27,6 @@ const race = {
         mobility:-40,
     },
     {   
-        //i went with Asian rather than Pacific Islander
         id:'Asian',
         description:'A person having origins in any of the original peoples of the Far East, Southeast Asia, or the Indian Subcontinent, including, for example, Cambodia, China, India, Japan, Korea, Malaysia, Pakistan, the Philippine Islands, Thailand, and Vietnam; has no Hispanic or Latino origins.',
         result:'',
@@ -70,7 +69,7 @@ const genderIdentity = {
 
     id:'Gender',
     title:'Gender Identity',
-    image:'Gender-Diversity.jpg',
+    image:'gender.jpeg',
     description:'',
     
     choices:[{
@@ -119,8 +118,8 @@ const environmentalConditions = {
 
 
     id:'Geographic-Environmental-Conditions',
-    title:"Environments of Success",
-    image:'Geography.jpg',
+    title: 'Environments of Success',
+    image:'location.jpeg',
     description:'',
     
     choices:[{
@@ -161,7 +160,7 @@ const environmentalConditions = {
 const educationEvent = {
     id:'EducationEvent',
     title:'Degrees of Education',
-    image:'',
+    image:'education.jpeg',
     description:`Research shows that the greatest single correlation of high income is the education level of one's parents.
     The Fed study showed that income, earnings, and wealth increased with education levels.`,
     choices:[{
@@ -215,7 +214,7 @@ const educationEvent = {
 const trafficEvent = {
     id:'traffic-event',
     title:'Traffic Stop',
-    image:'',
+    image:'traffic.jpeg',
     description:`You are driving down the highway. You are driving the speed limit, your car is fully functional and your registration, insurance, and drivers liscense are valid. You are preparing to take the next exit, when you look in your rear view mirror and notice that a police officer is following you. The officer, turns on their lights and sirens and in accordance with the law you pull your car over to the side of the road as quickly as you can and in a safe location. The officer approaches your vehicle and you roll down the window. The officer, tells you that you were speeding. How do you respond?`,
     choices:[{
         id:'comply',
@@ -281,7 +280,7 @@ const medicalEvent = {
 const housingEvent = {
     id:'housing-choice',
     title:'',
-    image:'',
+    image:'housing.jpeg',
     description:'You just got a job in a new city and need to find housing. There are a number of options, what do you do?',
     choices:[{
         id:'roommate',
@@ -310,7 +309,7 @@ const housingEvent = {
 const employementEvent = {
     id:'employement-issue',
     title:'',
-    image:'',
+    image:'employment.jpeg',
     description:'You are working at your desk at a new job when your boss asks you to come into their office. As you take a seat, they start telling you about an opportunity that will increase your earnings, dramatically, overnight. It sounds too good to be true. As you listen, you realize your bosses proposal is not only against company HR policy, but this "opportunity" is against the law. Your boss hints that if you decline you will likely be fired. What do you do?',
     choices:[{
         id:'accept',
